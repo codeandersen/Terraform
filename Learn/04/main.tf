c7db6b0a36c9b6937bf499908a31de3a64cb19cc
Hans Christian Andersen <hc@hcconsult.dk>
2023-10-17 22:23:52 +0200

    Changes to code

---
+++ Learn/04/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-test-linux-vm"
+  location = var.region
+}
+#test
+resource "azurerm_virtual_network" "example" {
+  name                = "corp"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.region
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "virtual_machines"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "tux-nic01"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                            = "tux01"
+  resource_group_name             = azurerm_resource_group.example.name
+  location                        = var.region
+  size                            = "Standard_F2"
+  admin_password                  = "dsfgsd#¤34&%_23"
+  disable_password_authentication = false
+  admin_username                  = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
