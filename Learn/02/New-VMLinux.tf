f8d93c759df306df5c9620eb24d3defb44bd3a26
Hans Christian Andersen <az-admin-apento-hca@terma.com>
2023-10-17 09:09:02 +0200

    Add

---
+++ Learn/02/New-VMLinux.tf
@@ -0,0 +1,59 @@
+provider "azurerm" {
+  features {} 
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-test-linux-vm"
+  location = "West Europe"
+}
+#test
+resource "azurerm_virtual_network" "example" {
+  name                = "corp"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "virtual_machines"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "tux-nic01"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "tux01"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_password      = "dsfgsd#¤34&%_23"
+  disable_password_authentication = false
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
